bf57a9487424ff0a7067c51aefaf145b548002bc
marknemenzo <mlnemenzo@gmail.com>
2018-05-16 12:33:25 -0700

    completed single item view mostly

--- src/components/single_item.js
+++ src/components/single_item.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';  
+import { connect } from 'react-redux';
+import { getSingleItem } from '../actions'; 
 
 class SingleItem extends Component {
 
     componentDidMount() {
-        console.log("Single Props: ", this.props);
+        
+
+        this.props.getSingleItem(this.props.match.params.id);
+
     }
     
     render() {
+        console.log("Single Props: ", this.props);
+        const { title, details } = this.props.item;
+
         return (
             <div>
-                <h1 className = "center">All the single items</h1>
+                <h1 className = "center">To Do Item</h1>
+                <div className= "row center">
+                    <Link to ="/" className ="btn blue-grey">View Full List</Link>
+                </div>
+                <div className = "row center">  
+                    <h4>{title}</h4>
+                    <p>{details}</p>
+                </div>
             </div>
         )
     }
 }
 
-export default SingleItem;
+// display time completed, time created, and if completed   const time = new Date(time generated); 
+// add 2 buttons, one toggles whethere its completed or not, (change complete to "true")
+//change text on button whether its completed or not (extra)
+//2nd button: delete button it should redirect you back in the list, same as in the add item, which it directs you back to the list.
+
+function mapStateToProps(state) {
+    return {
+        item: state.list.single
+    }
+}
+
+export default connect(mapStateToProps, {getSingleItem: getSingleItem})(SingleItem);
